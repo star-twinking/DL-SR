7af76c25748806214b5162719dc682f88a1c4b3b
star-twinking <40950719+star-twinking@users.noreply.github.com>
2021-06-10 19:49:11 +0800

    DFGAN model

--- PytorchImplementation/models/DFGAN.py
+++ PytorchImplementation/models/DFGAN.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-import torch.fft  # This import is needed for fourier transform and using nn.fft.fftn would outcome bugs
+import torch.fft
 from torchsummary import summary
 
 
@@ -34,7 +34,7 @@ class FCAB(nn.Module):
             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True)
         )
-        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(128, 128))
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
         self.conv_relu2 = nn.Sequential(
             nn.Conv2d(64, 4, kernel_size=1, stride=1, padding=0),
             nn.ReLU()
@@ -43,6 +43,7 @@ class FCAB(nn.Module):
             nn.Conv2d(4, 64, kernel_size=1, stride=1, padding=0),
             nn.Sigmoid()
         )
+
 
     def forward(self, x, gamma=0.8):
         x0 = x
@@ -56,15 +57,14 @@ class FCAB(nn.Module):
         x = self.avgpool(x)
         x = self.conv_relu2(x)
         x = self.conv_sig(x)
+        # print(x.shape)
+        # print(x1.shape)
         x = x1 * x
         output = x + x0
         return output
 
 
 class ResidualGroup(nn.Module):
-    '''
-    The Residual Group
-    '''
     def __init__(self, n_fcab=4):
         super(ResidualGroup, self).__init__()
         FCABs = []
@@ -79,9 +79,9 @@ class ResidualGroup(nn.Module):
         return output
 
 
-class DFCAN(nn.Module):
-    def __init__(self, input_shape, scale=2, size_psc=128):
-        super(DFCAN, self).__init__()
+class Generator(nn.Module):
+    def __init__(self, input_shape, scale=2):
+        super(Generator, self).__init__()
         self.conv_gelu1 = nn.Sequential(
             nn.Conv2d(input_shape, 64, kernel_size=3, stride=1, padding=1),
             nn.GELU()
@@ -112,15 +112,79 @@ class DFCAN(nn.Module):
         return output
 
 
-# 测试模型 Test the model
+# ---------------------- Discriminator part -------------------------
+class convblock(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(convblock, self).__init__()
+        self.double_conv_relu = nn.Sequential(
+            nn.Conv2d(input_size, output_size, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(output_size, output_size, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(inplace=True)
+        )
+    def forward(self,x):
+        return self.double_conv_relu(x)
+
+
+
+class Discriminator(nn.Module):
+    def __init__(self, input_shape, ):
+        super(Discriminator, self).__init__()
+
+        self.conv_leak = nn.Sequential(
+            nn.Conv2d(input_shape, 32, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU()
+        )
+        self.conv1 = convblock(32, 32)
+        self.conv2 = convblock(32, 64)
+        self.conv3 = convblock(64, 128)
+        self.conv4 = convblock(128, 256)
+        self.conv5 = convblock(256, 512)
+        self.avgpool = nn.AdaptiveAvgPool2d(1) # global average pool在pytorch里面是这个
+        self.linear = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
+    def forward(self,x):
+        x = self.conv_leak(x)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        print(x.shape)
+        x = self.avgpool(x)
+        output = self.linear(x)
+        return output
+
+
+# # 测试Generator模型
+# # when practicing the model, it should be commented out
+# if __name__ == '__main__':
+#     x = torch.rand(1,6,128,128)
+#     model = Generator(input_shape=x.size()[1])
+#     y = model(x)
+#     print('Output shape:', y.shape)
+#     # 转为GPU
+#     # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#     # model.to(device)
+#     if torch.cuda.is_available():
+#         model.cuda()
+#     summary(model,input_size=(6,128,128))
+
+# 测试Discriminator模型(Test the Dsicriminator model),
+# when practicing the model, it should be commented out
 if __name__ == '__main__':
-    x = torch.rand(1, 6, 128, 128)
-    model = DFCAN(input_shape=x.size()[1])
+    x = torch.rand(1,6,128,128)
+    model = Discriminator(input_shape=x.size()[1])
     y = model(x)
     print('Output shape:', y.shape)
-    # Turn to GPU
+    # 转为GPU
     # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     # model.to(device)
     if torch.cuda.is_available():
         model.cuda()
-    summary(model, input_size=(6, 128, 128))
+    summary(model,input_size=(6,128,128))
